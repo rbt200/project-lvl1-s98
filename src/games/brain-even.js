f390fd454f13b8059589f681f9ddfdd112325269
John Smith <rovaro@list.ru>
2017-05-20 16:57:43 +0400

    add logic

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,7 +1,43 @@
 import readlineSync from 'readline-sync';
 
-const introEven = () => { 
-  console.log('sdfasdfasdf');
+let counter = 0;
+let userName = "";
+
+
+const introEven = function introEven() {
+console.log('Welcome to Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".\n');
+userName = _readlineSync2.default.question('May I have your name? ');
+console.log('Hello ' + userName + '!\n');
+
+logic(counter);
+
 };
-export default introEven;
+
+
+const logic = (c) => {
+
+const randomNumber = getRandomInt(0,10000000);
+const answer = _readlineSync2.default.question(`Qustion: ${randomNumber}  `);
+const trueResult = isEven(randomNumber);
+
+if(answer.localeCompare(trueResult) === 0){
+counter++;
+if(counter > 3){console.log(`Congratulations, ${userName}`);return;}
+logic(++counter);
+}
+else{
+
+console.log(`${answer} was wrong answer. Correct answer was ${trueResult}.\nLet us try again, ${userName}`);
+return;
+}
+
+}; 
+
+const isEven = (n) => n % 2 === 0 ? 'yes' : 'no';
+
+const getRandomInt = (min,max) => Math.floor(Math.random() * (max - min)) + min;
+
+
+exports.default = introEven;
 
