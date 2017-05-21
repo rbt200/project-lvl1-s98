a5ceb71c077aaa189a362eea8905f82150d56fa2
John Smith <rovaro@list.ru>
2017-05-21 16:31:42 +0400

    Update brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,7 +1,58 @@
 import readlineSync from 'readline-sync';
-import { cons, car, cdr, toString } from 'hexlet-pairs';
+import getRandomInt from '../random-int';
 
 let userName = '';
+let counter = 0;
+
+const getAction = () => { 
+  const actions = ['+','-','*'];
+  const res = getRandomInt(0, 3);
+  return res;
+};
+
+const logic = () => {
+  const firstNum = getRandomInt(0, 10000000);
+  const secondNum = getRandomInt(0, 10000000);
+  const action = getAction();
+  let result ;
+  
+  console.log(`Qustion: ${firstNum} ${action} ${secondNum}`);
+  const answer = readlineSync.question(`Your answer: `);
+  
+  switch(action) {
+  case '+':  result = firstNum + secondNum;
+      break;
+  case '-':  result = firstNum - secondNum;
+      break;      
+  case '*': result = firstNum * secondNum;      
+      break;
+ }
+  
+  if (answer === result) {
+    counter += 1;
+    if (counter > 3) { console.log(`Congratulations, ${userName}`); return; }
+    logic(counter += 1);
+  } else {
+    console.log(`"${answer}" was wrong answer. Correct answer was "${result}".\nLet us try again, ${userName}`);
+  }
+  
+};
+
+
+
+const logicOld = () => {
+  const randomNumber = getRandomInt(0, 10000000);
+  const answer = readlineSync.question(`Qustion: ${randomNumber}  `);
+  const trueResult = isEven(randomNumber);
+
+  if (answer.localeCompare(trueResult) === 0) {
+    counter += 1;
+    if (counter > 3) { console.log(`Congratulations, ${userName}`); return; }
+    logic(counter += 1);
+  } else {
+    console.log(`"${answer}" was wrong answer. Correct answer was "${trueResult}".\nLet us try again, ${userName}`);
+  }
+};
 
 const introCalc = function introCalc() {
   console.log('Welcome to Brain Games!');
@@ -10,6 +61,7 @@ const introCalc = function introCalc() {
   console.log(`Hello ${userName}!\n`);
   console.log(`Question: `);
   
+  logic();
 };
 
 export default introCalc;
