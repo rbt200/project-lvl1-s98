10525638d0413f46fca105f5f0d5d0e74c82920b
John Smith <rovaro@list.ru>
2017-05-21 16:47:30 +0400

    Update brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -29,8 +29,9 @@ const logic = () => {
  }
   
   if (Number(answer) === Number(trueResult)) {
+    console.log(`Correct!`);
     counter += 1;
-    if (counter > 3) { console.log(`Congratulations, ${userName}`); }
+    if (counter > 3) { console.log(`Congratulations, ${userName}`); return; }
     logic(counter += 1);
   } else {
     console.log(`"${answer}" was wrong answer. Correct answer was "${trueResult}".\nLet us try again, ${userName}`);
