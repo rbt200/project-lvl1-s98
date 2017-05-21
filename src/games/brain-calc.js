70a5cd0a26d1354985affe5fe26a6933c6fe62a1
John Smith <rovaro@list.ru>
2017-05-21 15:06:44 +0400

    Create brain-calc.js

---
+++ src/games/brain-calc.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const introCalc = function introCalc() {
+  console.log('Welcome to Brain Games!');
+  console.log('What is the result of the expression?\n');
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!\n`);
+  
+};
+
+export default introCalc;
