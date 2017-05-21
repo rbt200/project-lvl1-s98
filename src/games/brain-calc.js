c9f76acc362723a9abddc89b7191d49a3e1f07f0
myname <myesoap@mail.ru>
2017-05-21 19:14:00 +0300

    linted

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -4,7 +4,7 @@ import getRandomInt from '../random-int';
 let userName = '';
 let counter = 0;
 
-const getAction = () => { 
+const getAction = () => {
   const actions = ['+', '-', '*'];
   const actionSingle = getRandomInt(0, 3);
   return actions[actionSingle];
@@ -15,37 +15,36 @@ const logic = () => {
   const secondNum = getRandomInt(1, 11);
   const action = getAction();
   let trueResult;
-  
   console.log(`Qustion: ${firstNum} ${action} ${secondNum}`);
-  const answer = readlineSync.question(`Your answer: `);  
+  const answer = readlineSync.question('Your answer: ');
+
   switch (action) {
-  case '+':  trueResult = firstNum + secondNum;
+    case '+': trueResult = firstNum + secondNum;
       break;
-  case '-':  trueResult = firstNum - secondNum;
-      break;      
-  case '*': trueResult = firstNum * secondNum;      
+    case '-': trueResult = firstNum - secondNum;
       break;
- }
-  
+    case '*': trueResult = firstNum * secondNum;
+      break;
+    default:
+  }
+
   if (Number(answer) === Number(trueResult)) {
-    console.log(`Correct!`);
+    console.log('Correct!');
     counter += 1;
     if (counter > 3) { console.log(`Congratulations, ${userName}`); return; }
     logic(counter += 1);
   } else {
-    console.log(`"${answer}" was wrong answer. Correct answer was "${trueResult}".\nLet us try again, ${userName}`);
+    console.log(`'${answer}' was wrong answer. Correct answer was '${trueResult}'.\nLet us try again, ${userName}`);
   }
-  
 };
-
 
 const introCalc = function introCalc() {
   console.log('Welcome to Brain Games!');
   console.log('What is the result of the expression?\n');
   userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!\n`);
-  console.log(`Question: `);
-  
+  console.log('Question: ');
+
   logic();
 };
 
