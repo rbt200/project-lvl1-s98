a3e090b4bd7a0989511d8daf491387104f4a3225
John Smith <rovaro@list.ru>
2017-05-22 01:35:46 +0400

    add introGcd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import introCalc from '../src/games/brain-calc';
 import introEven from '../src/games/brain-even';
-import introNod from '../src/games/brain-nod';
+import introNod from '../src/games/brain-gcd';
 
 
 export const calc = () => introCalc();
 export const even = () => introEven();
-export const nod = () => introNod();
+export const gcd = () => introGcd();
