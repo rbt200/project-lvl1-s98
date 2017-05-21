de60614213631d640660e4c2b1b7013459b42ef7
John Smith <rovaro@list.ru>
2017-05-21 11:44:00 +0400

    Update brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -7,7 +7,7 @@ let userName = "";
 const introEven = function introEven() {
 console.log('Welcome to Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no".\n');
-userName = readlineSync.default.question('May I have your name? ');
+userName = readlineSync.question('May I have your name? ');
 console.log('Hello ' + userName + '!\n');
 
 logic(counter);
@@ -18,7 +18,7 @@ logic(counter);
 const logic = (c) => {
 
 const randomNumber = getRandomInt(0,10000000);
-const answer = readlineSync.default.question(`Qustion: ${randomNumber}  `);
+const answer = readlineSync.question(`Qustion: ${randomNumber}  `);
 const trueResult = isEven(randomNumber);
 
 if(answer.localeCompare(trueResult) === 0){
