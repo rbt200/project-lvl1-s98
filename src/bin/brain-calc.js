c623b1e398de98c705d8a27701b02d2a94cd5a9f
myname <myesoap@mail.ru>
2017-05-21 23:42:23 +0300

    lint

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
 import * as game from '..';
-import introCalc from '../games/brain-calc';
 
 game.calc();
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-import readlineSync from 'readline-sync';
 import introCalc from '../src/games/brain-calc';
 import introEven from '../src/games/brain-even';
 
-export const greeting = () => {
-  console.log('WELCOME to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  /* console.log(`Hello, ${name}`); */
-  return name;
-};
 
-export const calc = () => {
-  return introCalc();
-}
+export const calc = () => introCalc();
 
-export const even = () => {
-  return introEven();
-}
+export const even = () => introEven();
