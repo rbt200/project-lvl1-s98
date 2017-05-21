a146a7cd05409caad448bf213792f8bb7ec072ac
myname <myesoap@mail.ru>
2017-05-21 12:26:02 +0300

    linted

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,42 +1,33 @@
 import readlineSync from 'readline-sync';
 
 let counter = 0;
-let userName = "";
+let userName = '';
 
+const isEven = (n) => { return n % 2 === 0 ? 'yes' : 'no'; };
 
-const introEven = function introEven() {
-console.log('Welcome to Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".\n');
-userName = readlineSync.question('May I have your name? ');
-console.log('Hello ' + userName + '!\n');
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-logic(counter);
+const logic = () => {
+  const randomNumber = getRandomInt(0, 10000000);
+  const answer = readlineSync.question(`Qustion: ${randomNumber}  `);
+  const trueResult = isEven(randomNumber);
 
+  if (answer.localeCompare(trueResult) === 0) {
+    counter += 1;
+    if (counter > 3) { console.log(`Congratulations, ${userName}`); return; }
+    logic(counter += 1);
+  } else {
+    console.log(`"${answer}" was wrong answer. Correct answer was "${trueResult}".\nLet us try again, ${userName}`);
+  }
 };
 
+const introEven = function introEven() {
+  console.log('Welcome to Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!\n`);
 
-const logic = (c) => {
-
-const randomNumber = getRandomInt(0,10000000);
-const answer = readlineSync.question(`Qustion: ${randomNumber}  `);
-const trueResult = isEven(randomNumber);
-
-if(answer.localeCompare(trueResult) === 0){
-counter++;
-if(counter > 3){console.log(`Congratulations, ${userName}`);return;}
-logic(++counter);
-}
-else{
-
-console.log(`"${answer}" was wrong answer. Correct answer was "${trueResult}".\nLet us try again, ${userName}`);
-return;
-}
-
-}; 
-
-const isEven = (n) => n % 2 === 0 ? 'yes' : 'no';
-
-const getRandomInt = (min,max) => Math.floor(Math.random() * (max - min)) + min;
-
+  logic(counter);
+};
 
 export default introEven;
