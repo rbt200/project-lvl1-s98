7d1dda194e56fb049ced359c9492852a4fe2ade1
John Smith <rovaro@list.ru>
2017-05-22 01:26:04 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import introCalc from '../src/games/brain-calc';
 import introEven from '../src/games/brain-even';
+import introNod from '../src/games/brain-nod';
 
 
 export const calc = () => introCalc();
-
 export const even = () => introEven();
+export const nod = () => introNod();
