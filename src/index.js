4160f876dfc69d203d02d250ccf1aeb4abbf4b47
John Smith <rovaro@list.ru>
2017-05-22 04:07:02 +0400

    add brain-balance

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import introCalc from '../src/games/brain-calc';
 import introEven from '../src/games/brain-even';
 import introGcd from '../src/games/brain-gcd';
+import introBalance from '../src/games/brain-balance';
 
 
 export const calc = () => introCalc();
 export const even = () => introEven();
 export const gcd = () => introGcd();
+export const gcd = () => introBalance();
