d9b7b892bf79dfc3b8ee0e4145344209e932d4f9
myname <myesoap@mail.ru>
2017-05-22 15:43:16 +0300

    clean of errors

--- src/games/brain-balance.js
+++ src/games/brain-balance.js
@@ -5,36 +5,36 @@ let userName = '';
 let counter = 1;
 
 // return a unsorted balanced number
-const getBalancedNum = (arr) => {
+const getBalancedNum = (ar) => {
+  const arr = ar;
   arr.sort();
   const firstNum = arr[0];
-  const secondNum = arr[arr.length -1];
-  if (firstNum === secondNum || firstNum + 1 === secondNum) {
-  return arr.join('');
+  const secondNum = arr[arr.length - 1];
+  if (firstNum !== secondNum && firstNum + 1 !== secondNum) {
+    arr[0] += 1;
+    arr[arr.length - 1] -= 1;
+    getBalancedNum(arr);
   }
-  arr[0] += 1;
-  arr[arr.length - 1] -= 1;
-  getBalancedNum(arr);
+  return arr.join('');
 };
 
 const convertStrArrToIntArr = (n) => {
   // convert the number into a string
-  const str = n + '';
+  const str = n.toString();
   // convert the string into an array of chars
   const arr = str.split('');
   const newArr = [arr.length];
   for (let i = 0; i < arr.length; i += 1) {
-        newArr[i] = parseInt(arr[i]);
+    newArr[i] = parseInt(arr[i], 10);
   }
   return newArr;
 };
 
 const logic = () => {
-  let disbalancedNum = getRandomInt(0, 1001);
+  const disbalancedNum = getRandomInt(0, 101);
   const arr = convertStrArrToIntArr(disbalancedNum);
   console.log(`Question: ${disbalancedNum}`);
   const answer = readlineSync.question('Your answer: ');
-  const trueResult = typeof String(getBalancedNum(arr));
 
   if (answer === getBalancedNum(arr)) {
     console.log('Correct!');
