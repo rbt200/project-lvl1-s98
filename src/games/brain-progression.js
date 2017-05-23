c00e23d419a592253ea44551dd26c36e629d6303
John Smith <rovaro@list.ru>
2017-05-23 12:37:42 +0300

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,1 +1,26 @@
+import beginGame from '..';
+import getRandomInt from '../random-int';
 
+
+const createArithmeticProgression = () => {
+  
+};
+
+
+const getGameData = () => {
+  const randomNumber = getRandomInt(0, 100);
+  const trueResult = isEven(randomNumber);
+
+  const logicData = {
+    question: randomNumber.toString(),
+    trueResult: trueResult.toString(),
+  };
+  return logicData;
+};
+
+
+const task = 'What number is missing in this progression?';
+
+const even = () => beginGame(task, getGameData);
+
+export default even;
