0f023f8c4a7d731c373584ef82986372efc4daa6
myname <myesoap@mail.ru>
2017-05-23 17:16:42 +0300

    f

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,49 +1,26 @@
 import beginGame from '..';
 import getRandomInt from '../random-int';
 
-
-const createArithmeticProgression = () => {
-  const beginning = getRandomInt(1, 101);
-  const step = getRandomInt(1, 11);
-  const arr = [];
-  arr[0] = beginning;
-
-  const iter = (acc, count) => {
-    let counter = count;
-    if (counter === 10) { return acc; }
-    arr[counter] = arr[counter - 1] + step;
-    counter += 1;
-    return iter(arr, counter);
-  };
-  return iter(arr, 1);
-};
-
-const hideNumber = (arr) => {
-  const length = arr.length;
-  const rand = getRandomInt(0, length - 1);
-  return arr[rand];
-};
-
-const displayArithmeticProgression = (a, num) => {
-  const arr = a;
-  const pos = arr.indexOf(num);
-  arr[pos] = '..';
-  return arr.join(' ');
+const isNumberPrime = (n) => {
+  const origin = n;
+  for (let i = 2; i < origin; i += 1) {
+    if (origin % i === 0) { return false; }
+  }
+  return true;
 };
 
 const getGameData = () => {
-  const progression = createArithmeticProgression();
-  const trueResult = hideNumber(progression);
-  const displayProgression = displayArithmeticProgression(progression, trueResult);
+  const randomNumber = getRandomInt(2, 100);
+  const trueResult = isNumberPrime(randomNumber) === true ? 'yes' : 'no';
   const logicData = {
-    question: displayProgression.toString(),
+    question: randomNumber.toString(),
     trueResult: trueResult.toString(),
   };
   return logicData;
 };
 
 
-const task = 'What number is missing in this progression?';
+const task = 'Is the number prime? Answer "yes" or "no".';
 
 const even = () => beginGame(task, getGameData);
 
